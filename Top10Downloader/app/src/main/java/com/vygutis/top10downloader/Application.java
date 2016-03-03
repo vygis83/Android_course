589c6072bf2b137d0e557ae1fd41d9fdfc052f67
vygis83 <vygis83@gmail.com>
2016-03-03 16:13:46 +0000

    Top 10 Apps

---
+++ Top10Downloader/app/src/main/java/com/vygutis/top10downloader/Application.java
@@ -0,0 +1,35 @@
+package com.vygutis.top10downloader;
+
+/**
+ * Created by luksyvyg on 03/03/2016.
+ */
+public class Application {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
---
+++ Top10Downloader/app/src/main/java/com/vygutis/top10downloader/ParseApplications.java
@@ -0,0 +1,66 @@
+package com.vygutis.top10downloader;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+/**
+ * Created by luksyvyg on 03/03/2016.
+ */
+public class ParseApplications {
+
+    private String xmlData;
+    private ArrayList<Application> applications;
+
+    public ParseApplications(String xmlData) {
+        this.xmlData = xmlData;
+        applications = new ArrayList<>();
+    }
+
+    public ArrayList<Application> getApplications() {
+        return applications;
+    }
+
+    public boolean process() {
+        boolean status = true;
+        Application currentRecord;
+        boolean inEntry = false;
+        String textValue = "";
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new StringReader(this.xmlData));
+            int eventType = xpp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String tagName = xpp.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        Log.d("ParseApplications", "Starting tag for " + tagName);
+                        if(tagName.equalsIgnoreCase("entry")) {
+                            inEntry = true;
+                            currentRecord = new Application();
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        Log.d("ParseApplications", "Ending tag for " + tagName);
+                        break;
+                    default:
+
+                }
+                eventType = xpp.next();
+            }
+
+        } catch (Exception e) {
+            status = false;
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
