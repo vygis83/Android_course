19449b29f7d23fa0ba7e4cb7aa0db4b183b96799
Vygis <vygis83@gmail.com>
2016-03-08 09:05:17 +0000

    Youtube player finished

--- YoutubePlayer/app/src/main/java/com/vygutis/youtubeplayer/MainActivity.java
+++ YoutubePlayer/app/src/main/java/com/vygutis/youtubeplayer/MainActivity.java
@@ -1,12 +1,18 @@
 package com.vygutis.youtubeplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSingle;
+    private Button btnStandalone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        btnSingle = (Button) findViewById(R.id.btnPlaySingle);
+        btnStandalone = (Button) findViewById(R.id.btnStandalone);
+        btnSingle.setOnClickListener(this);
+        btnStandalone.setOnClickListener(this);
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +57,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case (R.id.btnPlaySingle):
+                intent = new Intent(MainActivity.this, YoutubeActivity.class);
+                break;
+            case (R.id.btnStandalone):
+                intent = new Intent(MainActivity.this, StandaloneActivity.class);
+                break;
+            default:
+                break;
+        }
+
+        if(intent != null) {
+            startActivity(intent);
+        }
     }
 }
